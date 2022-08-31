b29fdddc9b742100efe291ce25f8b503e3f8d19c
roddg86 <roddg@mail.ru>
2022-08-31 14:50:30 +0900

    if-else

---
+++ 3-if-else/src/Task1.kt
@@ -0,0 +1,13 @@
+/* Вывести на экран большее из двух целых чисел. */
+
+fun main(){
+    val number1: Int = 10
+    val number2: Int = 20
+
+    if (number1>number2){
+        println(number1)
+    }else{
+        println(number2)
+    }
+
+}
